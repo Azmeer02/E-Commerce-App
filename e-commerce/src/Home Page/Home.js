c0c1fb5c66182c97c1c0789f141900438d6271c2
Syed Azmeer Haider <syedazmeerhaider@gmail.com>
2021-12-15 18:15:07 +0500

    uploaded data

--- e-commerce/src/Home Page/Home.js
+++ e-commerce/src/Home Page/Home.js
@@ -1,108 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.jpg';
-import logo2 from './logo2.png'
-import './home.css';
-import fireDB from '../firebaseStore'
-import { Avatar } from '@mui/material';
-import { Button , Card} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import logo2 from "./logo2.png";
+import "./home.css";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import { Avatar } from "@mui/material";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function Home(){
-    // const [] = useState()
-    useEffect(()=>{
-        fireDB.child("Ads").on("value" , (snapshot)=>{
-            
-        })
-    },[])
-    return (
-        <>
-        <div className="navigation">
-            <img src={logo2} alt="logo"/>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" style={{
-                    float: "right" , marginRight: "75px" , marginTop: "12px"
-                }}/>
-                <Link to="/ad-area"><Button variant="outline-primary" style={{
-                    float: "right" , marginRight: "25px" , marginTop: "15px" , color: "white"
-                }}>Post An Ad</Button></Link>
-                <input type="text" placeholder="Search" style={{
-                    width: "500px" , height: "40px" , marginLeft: "425px" , marginTop: "80px" , 
-                    border: "2px solid grey" , borderRadius: "10px"
-                }}/>
-        </div>
-        <div className="body">
-            <Card style={{ width: '18rem' , marginLeft: "40px" , marginTop: "25px" , float: "left"}}>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Product 1</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="outline-primary">View Product</Button>
-                    <Button variant="primary" style={{float: "right" , width: "100px"}}>Buy Now</Button>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' , marginLeft: "45px" , marginTop: "25px" , float: "left"}}>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Product 2</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="outline-primary">View Product</Button>
-                    <Button variant="primary" style={{float: "right" , width: "100px"}}>Buy Now</Button>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' , marginRight: "45px" , marginTop: "25px" , float: "right"}}>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Product 4</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="outline-primary">View Product</Button>
-                    <Button variant="primary" style={{float: "right" , width: "100px"}}>Buy Now</Button>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' , marginRight: "40px" , marginTop: "25px" , float: "right"}}>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Product 3</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="outline-primary">View Product</Button>
-                    <Button variant="primary" style={{float: "right" , width: "100px"}}>Buy Now</Button>
-                </Card.Body>
-            </Card>
-            {/* <Card style={{ width: '18rem' , marginLeft: "40px" , marginTop: "25px" , float: "left"}}>
-                <Card.Img variant="top" src={logo} />
-                <Card.Body>
-                    <Card.Title>Product 5</Card.Title>
-                    <Card.Text>
-                        {ad.map((ad) => {
-                            return(
-                                <div>{ad.Description}</div>
-                            )
-                        })}
-                    </Card.Text>
-                    <Card.Text>
-                        {ad.map((ad) => {
-                            return(
-                                <div><b>Price:</b> {ad.Price}</div>
-                            )
-                        })}
-                    </Card.Text>
-                    <Button variant="outline-primary">View Product</Button>
-                    <Button variant="primary" style={{float: "right" , width: "100px"}}>Buy Now</Button>
-                </Card.Body>
-            </Card> */}
-        </div>
-        </>
-    )
+function Home() {
+  const [data, setData] = useState();
+    useEffect(() => {
+      const db = getDatabase();
+      const DataRef = ref(db, "userAds/");
+      onValue(DataRef, (snapshot) => {
+        const ads = snapshot.val();
+        console.log(ads);
+        if (snapshot.val() !== null) {
+          setData(ads);
+        } else {
+          setData({});
+        }
+      });
+    }, []);
+  return (
+    <>
+      <div className="navigation">
+        <img src={logo2} alt="logo" />
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"
+            style={{float: "right",marginRight: "75px",marginTop: "12px"}}/>
+          <Link to="/ad-area"><Button variant="outline-primary" style={{ 
+            float: "right",marginRight: "25px",marginTop: "15px",color: "white"}}>Post An Ad</Button></Link>
+        <input type="text" placeholder="Search" style={{
+          width: "500px", height: "40px", marginLeft: "425px", marginTop: "80px",
+          border: "2px solid grey", borderRadius: "10px"}}/>
+      </div>
+      <div className="body">
+        {data && Object.entries(data).map(item=>(
+          <Card style={{width: "18rem",marginLeft: "40px",marginTop: "25px",float: "left"}}>
+            <Card.Img variant="top" src={item[1].photos} />
+            <Card.Body>
+              <Card.Title>Product 1</Card.Title>
+              <Card.Text>
+                {item[1].description}
+              </Card.Text>
+              <Card.Text>
+                {item[1].price}
+              </Card.Text>
+              <Button variant="outline-primary">View Product</Button>
+              <Button variant="primary" style={{ float: "right", width: "100px" }}>Buy Now</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
--- e-commerce/src/firebaseStore.js
+++ e-commerce/src/firebaseStore.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/compat/app";
+import { getDatabase } from "firebase/database";
 import "firebase/database";
 
 const firebaseConfig = {
@@ -12,38 +13,6 @@ const firebaseConfig = {
 };
 
 const fireDB = firebase.initializeApp(firebaseConfig);
-export default fireDB.database().ref();
+const db = getDatabase(fireDB);
 
-// import { initializeApp } from "firebase/app";
-// import {getDatabase} from 'firebase/database';
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD6PNmZOXOg19MGLXJz-ZyR-x8kAII5B34",
-//     authDomain: "e-commerce-auth-3bd56.firebaseapp.com",
-//     databaseURL: "https://e-commerce-auth-3bd56-default-rtdb.firebaseio.com",
-//     projectId: "e-commerce-auth-3bd56",
-//     storageBucket: "e-commerce-auth-3bd56.appspot.com",
-//     messagingSenderId: "212170172586",
-//     appId: "1:212170172586:web:c0c35f8959e106205bc8d3",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const database = getDatabase(app);
-
-// export default database;
-
-// import firebase from "firebase/compat/app";
-// import {getDatabase} from 'firebase/database';
-
-// const app = firebase.initializeApp({
-//     apiKey: "AIzaSyD6PNmZOXOg19MGLXJz-ZyR-x8kAII5B34",
-//     authDomain: "e-commerce-auth-3bd56.firebaseapp.com",
-//     databaseURL: "https://e-commerce-auth-3bd56-default-rtdb.firebaseio.com",
-//     projectId: "e-commerce-auth-3bd56",
-//     storageBucket: "e-commerce-auth-3bd56.appspot.com",
-//     messagingSenderId: "212170172586",
-//     appId: "1:212170172586:web:c0c35f8959e106205bc8d3",
-// })
-
-// export const db = getDatabase(app);
-// export default db;
+export default db;
